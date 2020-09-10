641b73a32ec1c0c0ab0b8974be9ea9b283ce8a0d
Minseong Seo <cbnm943@gmail.com>
2020-09-10 17:26:19 +0900

    [Fix] Modify SearchPlayer.vue file for work together TIC

---
+++ frontend/src/components/Player/PlayerStatChart.vue
@@ -0,0 +1,67 @@
+<template>
+  <card type="default" header-classes="bg-transparent">
+    <div slot="header" class="row align-items-center">
+      <div class="col">
+        <h6 class="text-uppercase text-muted ls-1 mb-1">Performance</h6>
+        <h5 class="h3 mb-0">Total orders</h5>
+      </div>
+    </div>
+
+    <bar-chart :height="350" ref="barChart" :chart-data="redBarChart.chartData"></bar-chart>
+  </card>
+</template>
+<script>
+  // Charts
+  import * as chartConfigs from '@/components/Charts/config';
+  import BarChart from '@/components/Charts/BarChart';
+
+  export default {
+    components: {
+      BarChart
+    },
+    data() {
+      return {
+        bigLineChart: {
+          allData: [
+            [0, 20, 10, 30, 15, 40, 20, 60, 60],
+            [0, 20, 5, 25, 10, 30, 15, 40, 40]
+          ],
+          activeIndex: 0,
+          chartData: {
+            datasets: [],
+            labels: [],
+          },
+          extraOptions: chartConfigs.blueChartOptions,
+        },
+        redBarChart: {
+          chartData: {
+            labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            datasets: [{
+              label: 'Sales',
+              data: [25, 20, 30, 22, 17, 29]
+            }]
+          }
+        }
+      };
+    },
+    methods: {
+      initBigChart(index) {
+        let chartData = {
+          datasets: [
+            {
+              label: 'Performance',
+              data: this.bigLineChart.allData[index]
+            }
+          ],
+          labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        };
+        this.bigLineChart.chartData = chartData;
+        this.bigLineChart.activeIndex = index;
+      }
+    },
+    mounted() {
+      this.initBigChart(0);
+    }
+  };
+</script>
+<style></style>
