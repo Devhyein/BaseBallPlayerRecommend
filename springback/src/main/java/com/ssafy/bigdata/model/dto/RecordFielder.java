de03b11274d4ec97f092e25f4db10d8471014047
rain151112@naver.com <rain151112@naver.com>
2020-09-10 14:38:32 +0900

    [Feat] create player, pitcher, fielder  dto

---
+++ springback/src/main/java/com/ssafy/bigdata/model/dto/RecordFielder.java
@@ -0,0 +1,260 @@
+package com.ssafy.bigdata.model.dto;
+
+public class RecordFielder {
+    private int fielder_id;
+    private int player_id;
+    private int fielder_year;
+    private int fielder_g;
+    private int fielder_gs;
+    private float fielder_inn;
+    private int fielder_ch;
+    private int fielder_po;
+    private int fielder_a;
+    private int fielder_e;
+    private float fielder_fld;
+    private float fielder_rf9;
+    private float fielder_rng;
+    private float fielder_arm;
+    private float fielder_cs;
+    private float fielder_blk;
+    private float fielder_e_plus;
+    private float fielder_raa;
+    private float fielder_133;
+    private float fielder_posadj;
+    private float fielder_raawithadj;
+    private float fielder_waawoadj;
+    private float fielder_waawithadj;
+
+    public int getFielder_id() {
+        return fielder_id;
+    }
+
+    public void setFielder_id(int fielder_id) {
+        this.fielder_id = fielder_id;
+    }
+
+    public int getPlayer_id() {
+        return player_id;
+    }
+
+    public void setPlayer_id(int player_id) {
+        this.player_id = player_id;
+    }
+
+    public int getFielder_year() {
+        return fielder_year;
+    }
+
+    public void setFielder_year(int fielder_year) {
+        this.fielder_year = fielder_year;
+    }
+
+    public int getFielder_g() {
+        return fielder_g;
+    }
+
+    public void setFielder_g(int fielder_g) {
+        this.fielder_g = fielder_g;
+    }
+
+    public int getFielder_gs() {
+        return fielder_gs;
+    }
+
+    public void setFielder_gs(int fielder_gs) {
+        this.fielder_gs = fielder_gs;
+    }
+
+    public float getFielder_inn() {
+        return fielder_inn;
+    }
+
+    public void setFielder_inn(float fielder_inn) {
+        this.fielder_inn = fielder_inn;
+    }
+
+    public int getFielder_ch() {
+        return fielder_ch;
+    }
+
+    public void setFielder_ch(int fielder_ch) {
+        this.fielder_ch = fielder_ch;
+    }
+
+    public int getFielder_po() {
+        return fielder_po;
+    }
+
+    public void setFielder_po(int fielder_po) {
+        this.fielder_po = fielder_po;
+    }
+
+    public int getFielder_a() {
+        return fielder_a;
+    }
+
+    public void setFielder_a(int fielder_a) {
+        this.fielder_a = fielder_a;
+    }
+
+    public int getFielder_e() {
+        return fielder_e;
+    }
+
+    public void setFielder_e(int fielder_e) {
+        this.fielder_e = fielder_e;
+    }
+
+    public float getFielder_fld() {
+        return fielder_fld;
+    }
+
+    public void setFielder_fld(float fielder_fld) {
+        this.fielder_fld = fielder_fld;
+    }
+
+    public float getFielder_rf9() {
+        return fielder_rf9;
+    }
+
+    public void setFielder_rf9(float fielder_rf9) {
+        this.fielder_rf9 = fielder_rf9;
+    }
+
+    public float getFielder_rng() {
+        return fielder_rng;
+    }
+
+    public void setFielder_rng(float fielder_rng) {
+        this.fielder_rng = fielder_rng;
+    }
+
+    public float getFielder_arm() {
+        return fielder_arm;
+    }
+
+    public void setFielder_arm(float fielder_arm) {
+        this.fielder_arm = fielder_arm;
+    }
+
+    public float getFielder_cs() {
+        return fielder_cs;
+    }
+
+    public void setFielder_cs(float fielder_cs) {
+        this.fielder_cs = fielder_cs;
+    }
+
+    public float getFielder_blk() {
+        return fielder_blk;
+    }
+
+    public void setFielder_blk(float fielder_blk) {
+        this.fielder_blk = fielder_blk;
+    }
+
+    public float getFielder_e_plus() {
+        return fielder_e_plus;
+    }
+
+    public void setFielder_e_plus(float fielder_e_plus) {
+        this.fielder_e_plus = fielder_e_plus;
+    }
+
+    public float getFielder_raa() {
+        return fielder_raa;
+    }
+
+    public void setFielder_raa(float fielder_raa) {
+        this.fielder_raa = fielder_raa;
+    }
+
+    public float getFielder_133() {
+        return fielder_133;
+    }
+
+    public void setFielder_133(float fielder_133) {
+        this.fielder_133 = fielder_133;
+    }
+
+    public float getFielder_posadj() {
+        return fielder_posadj;
+    }
+
+    public void setFielder_posadj(float fielder_posadj) {
+        this.fielder_posadj = fielder_posadj;
+    }
+
+    public float getFielder_raawithadj() {
+        return fielder_raawithadj;
+    }
+
+    public void setFielder_raawithadj(float fielder_raawithadj) {
+        this.fielder_raawithadj = fielder_raawithadj;
+    }
+
+    public float getFielder_waawoadj() {
+        return fielder_waawoadj;
+    }
+
+    public void setFielder_waawoadj(float fielder_waawoadj) {
+        this.fielder_waawoadj = fielder_waawoadj;
+    }
+
+    public float getFielder_waawithadj() {
+        return fielder_waawithadj;
+    }
+
+    public void setFielder_waawithadj(float fielder_waawithadj) {
+        this.fielder_waawithadj = fielder_waawithadj;
+    }
+
+    @Override
+    public String toString() {
+        return "RecordFielder [fielder_133=" + fielder_133 + ", fielder_a=" + fielder_a + ", fielder_arm=" + fielder_arm
+                + ", fielder_blk=" + fielder_blk + ", fielder_ch=" + fielder_ch + ", fielder_cs=" + fielder_cs
+                + ", fielder_e=" + fielder_e + ", fielder_e_plus=" + fielder_e_plus + ", fielder_fld=" + fielder_fld
+                + ", fielder_g=" + fielder_g + ", fielder_gs=" + fielder_gs + ", fielder_id=" + fielder_id
+                + ", fielder_inn=" + fielder_inn + ", fielder_po=" + fielder_po + ", fielder_posadj=" + fielder_posadj
+                + ", fielder_raa=" + fielder_raa + ", fielder_raawithadj=" + fielder_raawithadj + ", fielder_rf9="
+                + fielder_rf9 + ", fielder_rng=" + fielder_rng + ", fielder_waawithadj=" + fielder_waawithadj
+                + ", fielder_waawoadj=" + fielder_waawoadj + ", fielder_year=" + fielder_year + ", player_id="
+                + player_id + "]";
+    }
+
+    public RecordFielder() {
+    }
+
+    public RecordFielder(int fielder_id, int player_id, int fielder_year, int fielder_g, int fielder_gs,
+            float fielder_inn, int fielder_ch, int fielder_po, int fielder_a, int fielder_e, float fielder_fld,
+            float fielder_rf9, float fielder_rng, float fielder_arm, float fielder_cs, float fielder_blk,
+            float fielder_e_plus, float fielder_raa, float fielder_133, float fielder_posadj, float fielder_raawithadj,
+            float fielder_waawoadj, float fielder_waawithadj) {
+        this.fielder_id = fielder_id;
+        this.player_id = player_id;
+        this.fielder_year = fielder_year;
+        this.fielder_g = fielder_g;
+        this.fielder_gs = fielder_gs;
+        this.fielder_inn = fielder_inn;
+        this.fielder_ch = fielder_ch;
+        this.fielder_po = fielder_po;
+        this.fielder_a = fielder_a;
+        this.fielder_e = fielder_e;
+        this.fielder_fld = fielder_fld;
+        this.fielder_rf9 = fielder_rf9;
+        this.fielder_rng = fielder_rng;
+        this.fielder_arm = fielder_arm;
+        this.fielder_cs = fielder_cs;
+        this.fielder_blk = fielder_blk;
+        this.fielder_e_plus = fielder_e_plus;
+        this.fielder_raa = fielder_raa;
+        this.fielder_133 = fielder_133;
+        this.fielder_posadj = fielder_posadj;
+        this.fielder_raawithadj = fielder_raawithadj;
+        this.fielder_waawoadj = fielder_waawoadj;
+        this.fielder_waawithadj = fielder_waawithadj;
+    }
+
+    
+
+}
---
+++ springback/src/main/java/com/ssafy/bigdata/model/dto/RecordPitcher.java
@@ -0,0 +1,344 @@
+package com.ssafy.bigdata.model.dto;
+
+public class RecordPitcher {
+    private int pitcher_id;
+    private int player_id;
+    private int pitcher_year;
+    private int pitcher_g;
+    private int pitcher_cg;
+    private int pitcher_sho;
+    private int pitcher_gs;
+    private int pitcher_w;
+    private int pitcher_l;
+    private int pitcher_sv;
+    private int pitcher_hld;
+    private float pitcher_ip;
+    private int pitcher_r;
+    private int pitcher_er;
+    private int pitcher_h;
+    private int pitcher_2b;
+    private int pitcher_3b;
+    private int pitcher_homerun;
+    private int pitcher_bb;
+    private int pitcher_ibb;
+    private int pitcher_hbp;
+    private int pitcher_so;
+    private int pitcher_bk;
+    private int pitcher_wp;
+    private float pitcher_era;
+    private float pitcher_fip;
+    private float pitcher_whip;
+    private float pitcher_era_plus;
+    private float pitcher_fip_plus;
+    private float pitcher_war;
+    private float pitcher_wpa;
+
+    
+
+    public int getPitcher_id() {
+        return pitcher_id;
+    }
+
+    public void setPitcher_id(int pitcher_id) {
+        this.pitcher_id = pitcher_id;
+    }
+
+    public int getPlayer_id() {
+        return player_id;
+    }
+
+    public void setPlayer_id(int player_id) {
+        this.player_id = player_id;
+    }
+
+    public int getPitcher_year() {
+        return pitcher_year;
+    }
+
+    public void setPitcher_year(int pitcher_year) {
+        this.pitcher_year = pitcher_year;
+    }
+
+    public int getPitcher_g() {
+        return pitcher_g;
+    }
+
+    public void setPitcher_g(int pitcher_g) {
+        this.pitcher_g = pitcher_g;
+    }
+
+    public int getPitcher_cg() {
+        return pitcher_cg;
+    }
+
+    public void setPitcher_cg(int pitcher_cg) {
+        this.pitcher_cg = pitcher_cg;
+    }
+
+    public int getPitcher_sho() {
+        return pitcher_sho;
+    }
+
+    public void setPitcher_sho(int pitcher_sho) {
+        this.pitcher_sho = pitcher_sho;
+    }
+
+    public int getPitcher_gs() {
+        return pitcher_gs;
+    }
+
+    public void setPitcher_gs(int pitcher_gs) {
+        this.pitcher_gs = pitcher_gs;
+    }
+
+    public int getPitcher_w() {
+        return pitcher_w;
+    }
+
+    public void setPitcher_w(int pitcher_w) {
+        this.pitcher_w = pitcher_w;
+    }
+
+    public int getPitcher_l() {
+        return pitcher_l;
+    }
+
+    public void setPitcher_l(int pitcher_l) {
+        this.pitcher_l = pitcher_l;
+    }
+
+    public int getPitcher_sv() {
+        return pitcher_sv;
+    }
+
+    public void setPitcher_sv(int pitcher_sv) {
+        this.pitcher_sv = pitcher_sv;
+    }
+
+    public int getPitcher_hld() {
+        return pitcher_hld;
+    }
+
+    public void setPitcher_hld(int pitcher_hld) {
+        this.pitcher_hld = pitcher_hld;
+    }
+
+    public float getPitcher_ip() {
+        return pitcher_ip;
+    }
+
+    public void setPitcher_ip(float pitcher_ip) {
+        this.pitcher_ip = pitcher_ip;
+    }
+
+    public int getPitcher_r() {
+        return pitcher_r;
+    }
+
+    public void setPitcher_r(int pitcher_r) {
+        this.pitcher_r = pitcher_r;
+    }
+
+    public int getPitcher_er() {
+        return pitcher_er;
+    }
+
+    public void setPitcher_er(int pitcher_er) {
+        this.pitcher_er = pitcher_er;
+    }
+
+    public int getPitcher_h() {
+        return pitcher_h;
+    }
+
+    public void setPitcher_h(int pitcher_h) {
+        this.pitcher_h = pitcher_h;
+    }
+
+    public int getPitcher_2b() {
+        return pitcher_2b;
+    }
+
+    public void setPitcher_2b(int pitcher_2b) {
+        this.pitcher_2b = pitcher_2b;
+    }
+
+    public int getPitcher_3b() {
+        return pitcher_3b;
+    }
+
+    public void setPitcher_3b(int pitcher_3b) {
+        this.pitcher_3b = pitcher_3b;
+    }
+
+    public int getPitcher_homerun() {
+        return pitcher_homerun;
+    }
+
+    public void setPitcher_homerun(int pitcher_homerun) {
+        this.pitcher_homerun = pitcher_homerun;
+    }
+
+    public int getPitcher_bb() {
+        return pitcher_bb;
+    }
+
+    public void setPitcher_bb(int pitcher_bb) {
+        this.pitcher_bb = pitcher_bb;
+    }
+
+    public int getPitcher_ibb() {
+        return pitcher_ibb;
+    }
+
+    public void setPitcher_ibb(int pitcher_ibb) {
+        this.pitcher_ibb = pitcher_ibb;
+    }
+
+    public int getPitcher_hbp() {
+        return pitcher_hbp;
+    }
+
+    public void setPitcher_hbp(int pitcher_hbp) {
+        this.pitcher_hbp = pitcher_hbp;
+    }
+
+    public int getPitcher_so() {
+        return pitcher_so;
+    }
+
+    public void setPitcher_so(int pitcher_so) {
+        this.pitcher_so = pitcher_so;
+    }
+
+    public int getPitcher_bk() {
+        return pitcher_bk;
+    }
+
+    public void setPitcher_bk(int pitcher_bk) {
+        this.pitcher_bk = pitcher_bk;
+    }
+
+    public int getPitcher_wp() {
+        return pitcher_wp;
+    }
+
+    public void setPitcher_wp(int pitcher_wp) {
+        this.pitcher_wp = pitcher_wp;
+    }
+
+    public float getPitcher_era() {
+        return pitcher_era;
+    }
+
+    public void setPitcher_era(float pitcher_era) {
+        this.pitcher_era = pitcher_era;
+    }
+
+    public float getPitcher_fip() {
+        return pitcher_fip;
+    }
+
+    public void setPitcher_fip(float pitcher_fip) {
+        this.pitcher_fip = pitcher_fip;
+    }
+
+    public float getPitcher_whip() {
+        return pitcher_whip;
+    }
+
+    public void setPitcher_whip(float pitcher_whip) {
+        this.pitcher_whip = pitcher_whip;
+    }
+
+    public float getPitcher_era_plus() {
+        return pitcher_era_plus;
+    }
+
+    public void setPitcher_era_plus(float pitcher_era_plus) {
+        this.pitcher_era_plus = pitcher_era_plus;
+    }
+
+    public float getPitcher_fip_plus() {
+        return pitcher_fip_plus;
+    }
+
+    public void setPitcher_fip_plus(float pitcher_fip_plus) {
+        this.pitcher_fip_plus = pitcher_fip_plus;
+    }
+
+    public float getPitcher_war() {
+        return pitcher_war;
+    }
+
+    public void setPitcher_war(float pitcher_war) {
+        this.pitcher_war = pitcher_war;
+    }
+
+    public float getPitcher_wpa() {
+        return pitcher_wpa;
+    }
+
+    public void setPitcher_wpa(float pitcher_wpa) {
+        this.pitcher_wpa = pitcher_wpa;
+    }
+
+    @Override
+    public String toString() {
+        return "RecordPitcher [pitcher_2b=" + pitcher_2b + ", pitcher_3b=" + pitcher_3b + ", pitcher_bb=" + pitcher_bb
+                + ", pitcher_bk=" + pitcher_bk + ", pitcher_cg=" + pitcher_cg + ", pitcher_er=" + pitcher_er
+                + ", pitcher_era=" + pitcher_era + ", pitcher_era_plus=" + pitcher_era_plus + ", pitcher_fip="
+                + pitcher_fip + ", pitcher_fip_plus=" + pitcher_fip_plus + ", pitcher_g=" + pitcher_g + ", pitcher_gs="
+                + pitcher_gs + ", pitcher_h=" + pitcher_h + ", pitcher_hbp=" + pitcher_hbp + ", pitcher_hld="
+                + pitcher_hld + ", pitcher_homerun=" + pitcher_homerun + ", pitcher_ibb=" + pitcher_ibb
+                + ", pitcher_id=" + pitcher_id + ", pitcher_ip=" + pitcher_ip + ", pitcher_l=" + pitcher_l
+                + ", pitcher_r=" + pitcher_r + ", pitcher_sho=" + pitcher_sho + ", pitcher_so=" + pitcher_so
+                + ", pitcher_sv=" + pitcher_sv + ", pitcher_w=" + pitcher_w + ", pitcher_war=" + pitcher_war
+                + ", pitcher_whip=" + pitcher_whip + ", pitcher_wp=" + pitcher_wp + ", pitcher_wpa=" + pitcher_wpa
+                + ", pitcher_year=" + pitcher_year + ", player_id=" + player_id + "]";
+    }
+
+    public RecordPitcher() {
+    }
+
+    public RecordPitcher(int pitcher_id, int player_id, int pitcher_year, int pitcher_g, int pitcher_cg,
+            int pitcher_sho, int pitcher_gs, int pitcher_w, int pitcher_l, int pitcher_sv, int pitcher_hld,
+            float pitcher_ip, int pitcher_r, int pitcher_er, int pitcher_h, int pitcher_2b, int pitcher_3b,
+            int pitcher_homerun, int pitcher_bb, int pitcher_ibb, int pitcher_hbp, int pitcher_so, int pitcher_bk,
+            int pitcher_wp, float pitcher_era, float pitcher_fip, float pitcher_whip, float pitcher_era_plus,
+            float pitcher_fip_plus, float pitcher_war, float pitcher_wpa) {
+        this.pitcher_id = pitcher_id;
+        this.player_id = player_id;
+        this.pitcher_year = pitcher_year;
+        this.pitcher_g = pitcher_g;
+        this.pitcher_cg = pitcher_cg;
+        this.pitcher_sho = pitcher_sho;
+        this.pitcher_gs = pitcher_gs;
+        this.pitcher_w = pitcher_w;
+        this.pitcher_l = pitcher_l;
+        this.pitcher_sv = pitcher_sv;
+        this.pitcher_hld = pitcher_hld;
+        this.pitcher_ip = pitcher_ip;
+        this.pitcher_r = pitcher_r;
+        this.pitcher_er = pitcher_er;
+        this.pitcher_h = pitcher_h;
+        this.pitcher_2b = pitcher_2b;
+        this.pitcher_3b = pitcher_3b;
+        this.pitcher_homerun = pitcher_homerun;
+        this.pitcher_bb = pitcher_bb;
+        this.pitcher_ibb = pitcher_ibb;
+        this.pitcher_hbp = pitcher_hbp;
+        this.pitcher_so = pitcher_so;
+        this.pitcher_bk = pitcher_bk;
+        this.pitcher_wp = pitcher_wp;
+        this.pitcher_era = pitcher_era;
+        this.pitcher_fip = pitcher_fip;
+        this.pitcher_whip = pitcher_whip;
+        this.pitcher_era_plus = pitcher_era_plus;
+        this.pitcher_fip_plus = pitcher_fip_plus;
+        this.pitcher_war = pitcher_war;
+        this.pitcher_wpa = pitcher_wpa;
+    }
+
+    
+}
