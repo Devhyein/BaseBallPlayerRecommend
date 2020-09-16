59a105280ce36e7980af2fa9353394a9ba6a24fe
rain151112@naver.com <rain151112@naver.com>
2020-09-16 14:43:42 +0900

    [Feat] fixed miss spell

--- springback/src/main/java/com/ssafy/bigdata/controller/TeamController.java
+++ springback/src/main/java/com/ssafy/bigdata/controller/TeamController.java
@@ -73,7 +73,7 @@ public class TeamController {
     private ResponseEntity<RestResponse> sendToPython(Map<String,Object> data) throws IOException {
         final RestResponse result = new RestResponse();
 
-        URL url = new URL("http://127.0.0.1:8000/api/recommend1?team_id=" + data.get("team_id") +"power=" + data.get("power")+ "&speed=" + data.get("speed")
+        URL url = new URL("http://127.0.0.1:8000/api/recommend1?team_id=" + data.get("team_id") +"&power=" + data.get("power")+ "&speed=" + data.get("speed")
             + "&contact=" + data.get("contact") + "&defense=" + data.get("defense") + "&shoulder=" + data.get("shoulder")
             + "&era=" + data.get("era") + "&health=" + data.get("health") + "&control=" + data.get("control")
             + "&stability=" + data.get("stability") + "&deterrent=" + data.get("deterrent"));
