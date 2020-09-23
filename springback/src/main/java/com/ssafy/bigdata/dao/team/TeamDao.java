3c7e525fbb4af30c8f7d35cd5820f905aaf30cfe
유지민 <ujimini11@gmail.com>
2020-09-23 13:47:50 +0900

    [Refactor] Delete unnecessary imports

--- springback/src/main/java/com/ssafy/bigdata/dao/team/TeamDao.java
+++ springback/src/main/java/com/ssafy/bigdata/dao/team/TeamDao.java
@@ -1,12 +1,11 @@
 package com.ssafy.bigdata.dao.team;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 import com.ssafy.bigdata.dto.Lineup;
-import com.ssafy.bigdata.dto.Player;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
