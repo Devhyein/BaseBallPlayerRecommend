03a951fa037aaf512bae453d9203df8dc57a3963
rain151112@naver.com <rain151112@naver.com>
2020-09-28 13:15:56 +0900

    [Feat] create token

--- springback/src/main/java/com/ssafy/bigdata/jwt/JwtTokenProvider.java
+++ springback/src/main/java/com/ssafy/bigdata/jwt/JwtTokenProvider.java
@@ -2,6 +2,7 @@ package com.ssafy.bigdata.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,9 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Component
@@ -25,31 +28,54 @@ public class JwtTokenProvider {
 
     // 토큰 유효시간 30분
     private long tokenValidTime = 30 * 60 * 1000L;
-    
+
     private final UserDetailsService userDetailsService;
 
     // 객체 초기화, secretKey를 Base64로 인코딩한다.
     // @PostConstruct
     // protected void init() {
-    //     secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    // secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     // }
 
     // JWT 토큰 생성
     public String createToken(String userPk) {
         Date now = new Date();
         System.out.println("****여기****");
-        Claims claims = Jwts.claims().setSubject(userPk).setIssuedAt(now).setExpiration(new Date(now.getTime() + tokenValidTime)); // JWT payload 에 저장되는 정보단위
-        System.out.println(claims);
-        try {
-            String jwt = Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256, secretKey.getBytes()).compact();
-            System.out.println("__________________________________________");
-            System.out.println(jwt);
-            System.out.println("*************************");
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+        Map<String, Object> payloads = new HashMap<>();
+        payloads.put("data", "My First JWT");
+        Long expiredTime = 1000 * 60L * 60L * 2L; // 토큰 유효 시간 (2시간)
 
-        }catch(Exception e){
-            System.out.println("또 여기");
-        } 
-        return null;
+        Date ext = new Date(); // 토큰 만료 시간
+        ext.setTime(ext.getTime() + expiredTime);
+        System.out.println("*************************");
+         // 토큰 Builder
+         JwtBuilder jwtB = Jwts.builder()
+         .setClaims(Jwts.claims().setSubject(userPk)) // Claims 설정
+         .setSubject(userPk) // 토큰 용도 
+         .setExpiration(ext) // 토큰 만료 시간 설정
+         .signWith(SignatureAlgorithm.HS256, secretKey.getBytes()); // HS256과 Key로 Sign
+         System.out.println(jwtB);
+         String jwt = jwtB.compact();
+          // 토큰 생성
+         System.out.println("*************************");
+         // Claims claims = Jwts.claims().setSubject(userPk).setIssuedAt(now).setExpiration(new Date(now.getTime() + tokenValidTime)); // JWT payload 에 저장되는 정보단위
+        // System.out.println(claims);
+        // try {
+        //     System.out.println(claims);
+        //     System.out.println(Jwts.builder().setClaims(claims));
+        //     System.out.println(Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256, secretKey.getBytes()));
+        //     String jwt = Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256, secretKey.getBytes()).compact();
+        //     System.out.println("__________________________________________");
+        //     System.out.println(jwt);
+        //     System.out.println("*************************");
+
+        // }catch(Exception e){
+        //     System.out.println("또 여기");
+        // } 
+        return jwt;
         // return Jwts.builder()
         //         .setClaims(claims) // 정보 저장
         //         .setIssuedAt(now) // 토큰 발행 시간 정보
